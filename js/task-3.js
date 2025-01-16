bf53763f0eb3c1f945078b082b424af043f9fdd8
Evgenii <zheniakyril@gmail.com>
2025-01-16 23:05:27 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@ function getElementWidth(content, padding, border) {
     let numberPadding = parseFloat(padding)
     let numberBorder = parseFloat (border)
     const result = numberContent + numberPadding * 2 + numberBorder * 2;
-    return result + "px";
+    return result;
 }
 console.log(getElementWidth("50px", "8px", "4px")); 
 console.log(getElementWidth("60px", "12px", "8.5px")); 
